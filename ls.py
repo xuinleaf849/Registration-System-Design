350bc5f8cd9f15fe52ff26a81c5f1b76ba7ff09e
xuinleaf849 <yunye@uchicago.edu>
2018-03-10 21:49:25 -0600

    Update ls.py

--- ls.py
+++ ls.py
@@ -7,45 +7,6 @@ import time
 import re
 
 
-# def get_mode(mode):
-# 	perm = ''
-# 	link = ''
-# 	#deal with whether it is a file or a directory
-# 	if stat.S_ISDIR(mode):
-# 		perm = 'd'
-# 	elif stat.S_ISLNK(mode):
-# 		perm = 'l'
-# 		link = os.readlink(mode)
-# 	elif stat.S_ISREG(mode):
-# 		perm = '-'
-# 	else:
-# 		sys.stderr.write("Cannot access %s: No such file or directory\n" %mode)
-# 		return
-# 	#deal with permissions
-# 	m = stat.S_IMODE(mode)
-# 	n = 100
-# 	while (n > 0):
-# 		temp = m
-# 		if m // n == '7':
-# 			perm = perm + 'rwx'
-#         elif m // n == '6':
-#         	perm = perm + 'rw-'
-#         elif m // n == '5':
-#             perm = perm + 'r-x'
-#         elif m // n == '4':
-#             perm = perm + 'r--'
-#         elif m // n == '3':
-#             perm = perm + '-wx'
-#         elif m // n == '2':
-#             perm = perm + '-w-'
-#         elif m // n == '1':
-#             perm = perm + '--x'
-#         elif m // n == '0':
-#         	perm = perm + '---'
-#         temp = temp % n
-#         n = n / 10
-#     return perm
-
 def lspython(file,fileName):
 	permission = stat.filemode(os.stat(file).st_mode)
 	link = os.stat(file).st_nlink
